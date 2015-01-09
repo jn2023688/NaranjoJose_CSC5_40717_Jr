761cfe3d54c7068e07fb119438cbca85a2a80ff7
jn2023688 <josenara2@gmail.com>
2015-01-08 20:14:22 -0800

    homework

---
+++ Hmwrk/Hello World!/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: owner
+ *
+ * Created on January 8, 2015, 7:59 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hello World!"<<endl;
+    return 0;
+}
+
