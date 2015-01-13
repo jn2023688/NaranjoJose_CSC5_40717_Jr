0e8cee471b87ba31d5a8ad8ee43486d5f012b6b3
jn2023688 <josenara2@gmail.com>
2015-01-13 10:55:23 -0800

    Signed-off-by: jn2023688 <josenara2@gmail.com>

---
+++ Class_Lab/LabSwap2/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose Naranjo
+ *
+ * Created on January 13, 2015, 10:22 AM
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+  
+ 
+int main(int argc, char** argv) {
+    //NAMING VARIABLES
+    int a=2;
+    int b=3;
+    //EQUATIONS EQUATIONS
+    a=a^b;
+    b=a^b;
+    a=a^b;
+    //SOME MORE OUTPUTS
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b<<endl;
+    return 0;
+}
+
