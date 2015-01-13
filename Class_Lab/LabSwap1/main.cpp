81570b201f2ffe60a1c56b3c060596a76beadba6
jn2023688 <josenara2@gmail.com>
2015-01-13 10:21:22 -0800

    Signed-off-by: jn2023688 <josenara2@gmail.com>

---
+++ Class_Lab/LabSwap1/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose Naranjo
+ *
+ * Created on January 13, 2015, 10:12 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+  
+
+int main(int argc, char** argv) {
+//NAME VARIEBLES
+    int a=2;
+    int b=3;
+    //TEMPERARAY CHANGE
+    float temp=a;
+    //output
+    cout<<"b = "<<temp<<endl;
+    return 0;
+}
+
